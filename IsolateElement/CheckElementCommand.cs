bd56c18079e05be6adace96a8d7460a9f3b16b3b
aryandubey25 <aryan.dubey@ncircletech.com>
2025-02-04 13:07:38 +0530

    Add project files.

---
+++ IsolateElement/CheckElementCommand.cs
@@ -0,0 +1,76 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+
+namespace IsolateElement
+{
+    [Transaction(TransactionMode.Manual)]
+    public class CheckElementCommand : IExternalCommand
+    {
+        private static List<ElementId> hiddenElements = new List<ElementId>();
+        private static bool isIsolated = false;
+
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            if (isIsolated)
+            {
+                
+                using (Transaction t = new Transaction(doc, "Unhide Elements"))
+                {
+                    t.Start();
+                    foreach (var hiddenElementId in hiddenElements)
+                    {
+                        doc.ActiveView.UnhideElements(new List<ElementId> { hiddenElementId });
+                    }
+                    hiddenElements.Clear();
+                    t.Commit();
+                }
+                isIsolated = false;
+                TaskDialog.Show("Isolate Element", "All elements restored.");
+            }
+            else
+            {
+                try
+                {
+                    Reference pickedRef = uidoc.Selection.PickObject(ObjectType.Element, "Select an element to isolate");
+                    Element selectedElement = doc.GetElement(pickedRef.ElementId);
+
+                   
+                    using (Transaction t = new Transaction(doc, "Isolate Element"))
+                    {
+                        t.Start();
+                        
+                        FilteredElementCollector collector = new FilteredElementCollector(doc, doc.ActiveView.Id)
+                            .WhereElementIsNotElementType()
+                            .Excluding(new List<ElementId> { selectedElement.Id });
+
+                        
+                        foreach (Element e in collector)
+                        {
+                            doc.ActiveView.HideElements(new List<ElementId> { e.Id });
+                            hiddenElements.Add(e.Id);
+                        }
+
+                        t.Commit();
+                    }
+
+                    isIsolated = true;
+                    TaskDialog.Show("Isolate Element", $"Element {selectedElement.Name} is now isolated.");
+                }
+                catch (Exception ex)
+                {
+                    message = ex.Message;
+                    return Result.Failed;
+                }
+            }
+
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ IsolateElement/Class1.cs
@@ -0,0 +1,32 @@
+﻿using Autodesk.Revit.UI;
+using System;
+using System.Reflection;
+using System.Windows.Media.Imaging;
+
+namespace IsolateElement
+{
+    public class Class1 : IExternalApplication
+    {
+        public Result OnStartup(UIControlledApplication application)
+        {
+            RibbonPanel ribbonPanel = application.CreateRibbonPanel("Isolate Element");
+
+            string thisAssemblyPath = Assembly.GetExecutingAssembly().Location;
+
+            PushButtonData buttonData = new PushButtonData("cmdIsolateElement", "Isolate Element", thisAssemblyPath, "IsolateElement.CheckElementCommand");
+            PushButton pushButton = ribbonPanel.AddItem(buttonData) as PushButton;
+
+            
+            Uri iconPath = new Uri(@"""C:\Users\Aryan.Dubey\Desktop\Revit Internship\Icons\Hierachy.png");
+            BitmapImage buttonImage = new BitmapImage(iconPath);
+            pushButton.LargeImage = buttonImage;
+
+            return Result.Succeeded;
+        }
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+    }
+}
